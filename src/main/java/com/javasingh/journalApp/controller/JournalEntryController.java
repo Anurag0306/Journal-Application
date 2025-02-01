5d0c3e4ca36101de0cf03c85b7dc14b914a91643
Anurag0306 <karamveer0306@gmail.com>
2025-02-01 16:59:13 +0530

    Journal APP 1day

---
+++ src/main/java/com/javasingh/journalApp/controller/JournalEntryController.java
@@ -0,0 +1,23 @@
+package com.javasingh.journalApp.controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+public class JournalEntryController {
+
+    private Map<Long,JournalEntry> journalEntries = new HashMap<>();
+
+    @GetMapping
+    public List<JournalEntry> getAll(){
+        return new ArrayList<>(journalEntries.values());
+    }
+    @PostMapping
+    public void createEntry(){
+        
+    }
+}
